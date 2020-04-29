8aaf7d487f6e0513dbcb79023fe1f95a06bf1fd6
Valeriy <requiemnosoul@gmail.com>
2020-04-30 03:17:28 +0500

    Add new AwpForm

--- awp/AwpForm.Designer.cs
+++ awp/AwpForm.Designer.cs
@@ -31,10 +31,16 @@ namespace awp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AwpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AwpForm";
+            this.Name = "AwpForm";
+            this.Text = "Automated Workplace";
+            this.ResumeLayout(false);
         }
 
         #endregion
